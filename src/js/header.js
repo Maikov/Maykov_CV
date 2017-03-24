9447fcad821d1f80222d166d0a188442f819f23e
Maikov <maikovap@mail.ru>
2017-03-24 10:55:27 +0200

    changed

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,7 @@
 	// HEADER
 
 $(document).ready(function() {
-  
+  // FOTO
   $('.foto').mouseenter(function(){
     $('#name').css('color','#fa8e0a');
   });
@@ -9,5 +9,33 @@ $(document).ready(function() {
   $('.foto').mouseleave(function(){
     $('#name').css('color','#fff');
   });
+
+// MENU
+	$('#hum').click(function(){
+    $('#menu').fadeIn(200);
+  });
+
+  // $('.btn').click(function(){
+  //   $('#menu').fadeOut();
+  // });
+
+  // $('#menu').mouseleave(function(){
+  //   $(this).fadeOut();
+  // });
+
+
+  function display_hide (menu) 
+{ 
+    if ($('#menu').css('display') == 'none') 
+        { 
+            $('#menu').fadeIn(200); 
+        } 
+    else 
+        {     
+            $('#menu').fadeOut(); 
+        } 
+} 
+
+
 });
 
